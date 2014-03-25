14c64e466cce4ed3ca7902cb133371f814c1617d
jay_win <gududanxing@gmail.com>
2014-03-25 15:10:18 +0800

    currency

--- Currency.cpp
+++ Currency.cpp
@@ -1,34 +1,27 @@
 // Currency.cpp
+
 #include <iostream>
-#include <string>
+#include "currency.h"
 
-using namespace std;
-
-enum sign{plus,minus};
-
-class Currency
-{
-public:
-	// 构造函数
-	Currency(sign s = plus,unsigned long d = 0,unsigned int c = 0);
-	// 析构函数
-	~Currency();
-	bool Set(sign s,unsigned long d,unsigned int c);
-	bool Set(float a);
-	sign Sign() const {return sgn;}
-	unsigned long Dollars() const {return dollars;}
-	unsigned int  Cents() const {return cents;}
-	Currency Add(const Currency& x) const;
-	Currency& Increment(const Currency& x);
-	void Output() const;
-
-private:
-	sign sgn;
-	unsigned long dollars;
-	unsigned int cents;
-};
 
 int main()
 {
+	Currency g,h(plus,3,50),i,j;
+	g.Set(minus,2,25);
+	i.Set(-6.45);
+	j = h.Add(g);
 
+	j.Output();
+	cout << endl;
+	i.Increment(g);
+	i.Output();
+	cout << endl;
+	j = i.Add(g).Add(h);
+	j.Output();
+	cout << endl;
+	j = i.Increment(g).Add(h);
+	j.Output();
+	cout << endl;
+	i.Output();
+	cout << endl;
 }
