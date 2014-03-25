980aceb495ad32e2d81525240165a1f081fdb4b2
jay_win <gududanxing@gmail.com>
2014-03-25 16:55:26 +0800

    运算符重载

--- Currency.cpp
+++ Currency.cpp
@@ -9,19 +9,27 @@ int main()
 	Currency g,h(plus,3,50),i,j;
 	g.Set(minus,2,25);
 	i.Set(-6.45);
-	j = h.Add(g);
-
-	j.Output();
+	//j = h.Add(g);
+	j = h + g;
+	//j.Output();
+	cout << j;
 	cout << endl;
-	i.Increment(g);
-	i.Output();
+	//i.Increment(g);
+	i += g;
+	//i.Output();
+	cout << i;
 	cout << endl;
-	j = i.Add(g).Add(h);
-	j.Output();
+	//j = i.Add(g).Add(h);
+	j = i + g + h;
+	//j.Output();
+	cout << j;
 	cout << endl;
-	j = i.Increment(g).Add(h);
-	j.Output();
+	//j = i.Increment(g).Add(h);
+	j +=g;
+	j +=h;
+	//j.Output();
+	cout << j;
 	cout << endl;
-	i.Output();
+	//i.Output();
 	cout << endl;
 }
